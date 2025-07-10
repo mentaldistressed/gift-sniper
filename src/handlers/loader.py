d8367f5815f53b33247d0f9f7a57533dfc6b0078
forgotten-xyz <93533394+forgotten-xyz@users.noreply.github.com>
2025-07-10 19:23:52 +0300

    str

--- src/handlers/loader.py
+++ src/handlers/loader.py
@@ -6,7 +6,7 @@ from aiogram import Router
 
 
 def get_all_routers() -> Generator[tuple[Router, str], None, None]:
-    for module_info in pkgutil.iter_modules([Path(__file__).parent]):
+    for module_info in pkgutil.iter_modules([str(Path(__file__).parent)]):
         module = importlib.import_module(f"src.handlers.{module_info.name}")
         if hasattr(module, 'router'):
             yield module.router, module.rname
